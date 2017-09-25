21a848960f00951ae54b3addd01d3da2a03d34bc
Simeon Ehrig <simeonehrig@web.de>
2017-09-25 14:08:45 +0200

    overwork runtime.cu
    - remove race condition
    - add code, to generate an little more sensible program

--- example_prog/runtime.cu
+++ example_prog/runtime.cu
@@ -2,37 +2,45 @@
 #include <cuda_runtime.h>
 
 __global__ void foo_device(int * n){
-	//int i = threadIdx.x;	
-	n[0] = 42;
+	int i = threadIdx.x;	
+	n[i] = 7*i;
 }
 
 int main(int argc, char const *argv[])
 {
-	int * device;
-	int host;
+    int * device;
+    int * host;
     cudaError_t error;
     
-	error = cudaMalloc( (void **) &device, sizeof(int));
+    host = new int[4];
+    error = cudaMalloc( (void **) &device, sizeof(int)*4);
     
     if (error != cudaSuccess)
     {
         std::cout << "cudaMalloc returned error " << cudaGetErrorString(error) << "\n";
     }
     
-	foo_device<<<1,20>>>(device);
+    foo_device<<<1,4>>>(device);
     
     error = cudaGetLastError();
     if (error != cudaSuccess)
     {
         std::cout << "kernel returned error " << cudaGetErrorString(error) << "\n";;
     }
-	error = cudaMemcpy(&host, device, sizeof(int), cudaMemcpyDeviceToHost);
 
+    error = cudaMemcpy(host, device, sizeof(int)*4, cudaMemcpyDeviceToHost);
     if (error != cudaSuccess)
     {
         std::cout << "cudaMemcyp returned error " << cudaGetErrorString(error) << "\n";;
     }
     
-	std::cout << "the cuda number is: " << host << std::endl;
-	return 0;
+    int sum = host[0] + host[1] + host[2] + host[3];
+
+    if(sum == 42){
+	std::cout << "The program works fine! The right anwser is: " << sum << std::endl; 
+    }else{
+	std::cout << "The answer is wrong. 42 was expected, but it is: " << sum << std::endl;
+    }
+
+    return 0;
 }
